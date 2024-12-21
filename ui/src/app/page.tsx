7682e8e9d2b5eaac7c5e2cea3b11cb582fa36c29
Frank Hereford <github@frankhereford.com>
2024-12-21 13:29:53 -0600

    whatever its rendering

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,3 +1,16 @@
+'use client';
+
+import Layout from '~/components/Layout';
+import Section from '~/components/Section';
+import Container from '~/components/Container';
+import Map from '~/components/Map';
+import Button from '~/components/Button';
+
+import styles from '../styles/Home.module.scss';
+const DEFAULT_CENTER = [38.907132, -77.036546]
+
+
+
 export default function HomePage() {
   
   return (
@@ -13,6 +26,24 @@ export default function HomePage() {
         width: '80vw',
         border: '1px solid black'
       }}>
+      
+          <Map className={styles.homeMap}  center={DEFAULT_CENTER} zoom={12}>
+            {({ TileLayer, Marker, Popup }) => (
+              <>
+                <TileLayer
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                />
+                <Marker position={DEFAULT_CENTER}>
+                  <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                  </Popup>
+                </Marker>
+              </>
+            )}
+          </Map>
+        
+
       </div>
     </div>
   );
