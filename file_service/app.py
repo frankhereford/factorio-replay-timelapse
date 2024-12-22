290a5090699c98d12a1e82dc0e45c0300810c1d9
Frank Hereford <github@frankhereford.com>
2024-12-22 17:30:27 -0600

    Update base zoom level in generate_still_tile function and add numpy to requirements

--- file_service/app.py
+++ file_service/app.py
@@ -5,6 +5,7 @@ import redis
 import base64
 import logging
 import os
+import numpy as np
 
 app = Flask(__name__)
 
@@ -86,7 +87,7 @@ def generate_still_tile(tick, zoom, x, y):
     cache_key = f'{label}:{tick}:{zoom}:{x}:{y}'
     cached_image = redis_client.get(cache_key)
 
-    base_zoom = 7
+    base_zoom = 8
 
     if cached_image:
         logging.debug(f'Cache hit 🎉 for key: {cache_key}')
