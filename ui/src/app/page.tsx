21aaf13eea4f10711d092534aee8d1ec880c497e
Frank Hereford <github@frankhereford.com>
2024-12-23 14:19:06 -0600

    Refactor homepage layout and integrate tRPC greeting

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,20 +1,36 @@
 import Link from "next/link";
 
-import 'leaflet/dist/leaflet.css';
-
-// import { LatestPost } from "~/app/_components/post";
+import { LatestPost } from "~/app/_components/post";
 import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
-  // const hello = await api.post.hello({ text: "from tRPC" });
-  // void api.post.getLatest.prefetch();
+  const hello = await api.post.hello({ text: "from tRPC" });
+
+  void api.post.getLatest.prefetch();
 
   return (
     <HydrateClient>
-      <main>
-        <div id="map" className="h-full h-screen border-indigo-700 bg-indigo-100">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+          <div className="flex flex-col items-center gap-2">
+            <p className="text-2xl text-white">
+              {hello ? hello.greeting : "Loading tRPC query..."}
+            </p>
+          </div>
+          <LatestPost />
         </div>
       </main>
     </HydrateClient>
   );
 }
+
+
+//   return (
+//     <HydrateClient>
+//       <main>
+//         <div id="map" className="h-full h-screen border-indigo-700 bg-indigo-100">
+//         </div>
+//       </main>
+//     </HydrateClient>
+//   );
+// }
