fb2021fd0ca8a1ab7a2e8c3c7a1ebccbcb3d5208
Frank Hereford <github@frankhereford.com>
2024-12-24 11:46:18 -0600

    Fix syntax by removing unnecessary semicolon in MapClient component

--- ui/src/app/_components/map_server.tsx
+++ ui/src/app/_components/map_server.tsx
@@ -8,7 +8,7 @@ export default async function MapServer() {
   return (
     <>
       <HydrateClient>
-        <MapClient ticks={ticks} />;
+        <MapClient ticks={ticks} />
       </HydrateClient>
     </>
   );
