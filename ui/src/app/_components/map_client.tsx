0e8af8c34f635813ec55e9a54f706e6a2f3af992
Frank Hereford <github@frankhereford.com>
2024-12-23 22:35:21 -0600

    Integrate API for tick availability in MapClient and MapServer; refactor state management and add interval for data refetching

--- ui/src/app/_components/map_client.tsx
+++ ui/src/app/_components/map_client.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { api } from "~/trpc/react";
 import { useState, useEffect } from "react";
 import LeafletMap from "~/app/_components/leaflet_map";
 import TickPicker from "~/app/_components/tick_picker";
@@ -8,12 +9,28 @@ interface MapClientProps {
   ticks: number[];
 }
 
-export default function MapClient({ ticks }: MapClientProps) {
+export default function MapClient({ ticks: initialTicks }: MapClientProps) {
+  const [available, refetchAvailable] = api.tick.available.useSuspenseQuery();
+  const [ticks, setTicks] = useState(initialTicks);
   const [tick, setTick] = useState(0);
 
   useEffect(() => {
-    console.log("Ticks prop changed:", ticks);
-  }, [ticks]);
+    console.log("Ticks prop changed:", initialTicks);
+    setTicks(initialTicks);
+  }, [initialTicks]);
+
+  useEffect(() => {
+    console.log("available changed:", available);
+    setTicks(available);
+  }, [available]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      refetchAvailable.refetch();
+    }, 30000);
+
+    return () => clearInterval(interval);
+  }, [refetchAvailable]);
 
   return (
     <>
