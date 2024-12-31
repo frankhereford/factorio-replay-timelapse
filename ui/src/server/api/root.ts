b52b20e230ad6fd0e466f086009c5dcfcadc04c4
Frank Hereford <github@frankhereford.com>
2024-12-31 16:40:34 -0600

    Add surfaceRouter to appRouter for surface-related API endpoints

--- ui/src/server/api/root.ts
+++ ui/src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { postRouter } from "~/server/api/routers/post";
 import { tickRouter } from "~/server/api/routers/tick";
+import { surfaceRouter } from "~/server/api/routers/surface";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -10,6 +11,7 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 export const appRouter = createTRPCRouter({
   post: postRouter,
   tick: tickRouter,
+  surface: surfaceRouter,
 });
 
 // export type definition of API
